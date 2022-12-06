ef62a35ebe11bf0bd507cb65bcaacc92700e077f
Rafael Rodriguez <rafart1205@gmail.com>
2022-12-06 20:57:42 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+
+
+import { CarouselComponent } from './componets/carousel/carousel.component';
+import { NavbarComponent } from './componets/navbar/navbar.component';
+import { CardsComponent } from './componets/cards/cards.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarouselComponent,
+    NavbarComponent,
+    CardsComponent    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
